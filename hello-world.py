d928c7f0e42578d417bcca2ceab793156e35470e
Narodniacrit01 <writterv@ucsc.edu>
2024-10-10 22:03:22 -0700

    Session 1 prompt for coding journal 1

---
+++ hello-world.py
@@ -0,0 +1,19 @@
+# this program will 
+# write Tasha Ritter Von Stein!
+
+# This function tells
+# the computer to print
+# Tasha Ritter Von Stein!
+def PrintTashaRitterVonStein():
+    print("Tasha Ritter Von Stein!")
+    
+    
+# This defines our main()
+#function for our program
+def main():
+    PrintTashaRitterVonStein()
+    
+# when we run the program
+# this executes first
+if __name__=="__main__":
+    main()
